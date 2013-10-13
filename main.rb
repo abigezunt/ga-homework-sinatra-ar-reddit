d2c5274b2dc688c513331dc827b8684eaa013669
Abby Howell <abby.g.howell@gmail.com>
2013-10-13 15:24:20 -0400

    still working on it

--- main.rb
+++ main.rb
@@ -23,10 +23,9 @@ end
 class SubComment < Comment
 end
 
-
+@category_title = "Everything"
 
 get '/index/' do
-  @category_name = "Everything"
   @submissions = Submission.order("(up_votes - down_votes) DESC")
   erb :index
 end
@@ -36,8 +35,8 @@ get '/new' do
 end
 
 post '/new/create' do
-  Submission.create(title: params[:title])
-  redirect '/index/'
+  Category.create(title: params[:title])
+  redirect "/r/#{params[:title]}/index"
 end
 
 get '/newest' do
@@ -45,18 +44,35 @@ get '/newest' do
   erb :index
 end
 
-get '/r/:category/index' do
-  category = Category.where(title: params[:category]).first
-  @category = params[:category]
-  @submissions = category.submissions
-  erb :index
+get '/r/:category_title/index' do
+  @category_title = params[:category_title]
+  @category = Category.where(title: params[:category_title]).first
+  @submissions = @category.submissions
+  erb :category_index
 end
 
-get '/r/:category_id/:submission_id/comments' do
+post '/r/:category_name/new' do
+end
+
+get '/r/:category_name/:submission_id/comments' do
   @submission = Submission.find(params[:submission_id])
-  @category_id = params[:category_id]
-  @category_title = Category.find(params[:category_id]).title
+  @category = Category.where(title: params[:category_name]).first
+  @category_title = params[:category_name]
   erb :show_post.erb
+end
+
+post '/r/:category_id/:submission_id/up_vote' do
+  upvoted = Submission.find(params[:submission_id])
+  upvoted.up_votes += 1
+  upvoted.save
+  redirect '/index/'
+end
+
+post '/r/:category_id/:submission_id/down_vote' do
+  downvoted = Submission.find(params[:submission_id])
+  downvoted.down_votes += 1
+  downvoted.save
+  redirect '/index/'
 end
 
 post '/r/:category_id/:submission_id/add-comment' do
